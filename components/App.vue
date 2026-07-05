3a6a75cd6ed332a973ba816da7a1d392b3952b91
Mahdi Amolimoghaddam <54014061+beaconchain-horizon@users.noreply.github.com>
2026-07-05 19:08:27 +0330

    Add App.vue with HorizonChat component
    Signed-off-by: Mahdi Amolimoghaddam <54014061+beaconchain-horizon@users.noreply.github.com>

---
+++ components/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <HorizonChat :apiEndpoint="'https://api.deepseek.com/v1/chat/completions'" />
+</template>
+
+<script setup>
+import HorizonChat from './components/HorizonChat.vue'
+</script>
